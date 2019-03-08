f4d2d460f4c86fdd584d16a4a025acdd7055f63d
Dmitry G. Anderson <dandgerson@gmail.com>
2019-03-08 19:20:28 +0300

    solve unary-function-chainer.js

--- kata/unary-function-chainer.js
+++ kata/unary-function-chainer.js
@@ -1,5 +1,3 @@
-const chainer = (functions) => {
-  return () => { };
-};
+const chainer = functions => value => functions.reduce((acc, current) => current(acc), value);
 
 module.exports = chainer;
