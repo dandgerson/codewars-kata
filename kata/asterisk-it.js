cc90b80972ebc56003e17a9245c583277cc3162c
Dmitry G. Anderson <dandgerson@gmail.com>
2019-03-17 03:32:24 +0300

    try to solve asterisk it

---
+++ kata/asterisk-it.js
@@ -0,0 +1,59 @@
+const asteriscItArray = (n) => {
+  console.log(n);
+  let rawString = (n + '');
+  let resultString = '';
+  
+  if (Array.isArray(n)) {
+    let rawArray = rawString.split(',');
+    let absArray = rawArray.slice();
+      
+    if (n[0] < 0)  absArray[0] = -n[0];
+      
+    for (let i = 0; i < absArray.length; i += 1) {
+      resultString += absArray[i];
+      if (+absArray[i] % 2 === 0) resultString += '*';
+    }
+    
+    if (n[0] < 0) resultString = '-' + resultString;
+  } else {
+    let absString = rawString;
+  
+    if (n < 0) absString = rawString.slice(1);
+    
+    for (let i = 0; i < absString.length; i += 1) {
+      resultString += absString[i];
+      if (absString[i] % 2 === 0) resultString += '*';
+    }
+    
+    if (n < 0) resultString = '-' + resultString;
+  }
+  
+  if (resultString[resultString.length - 1] === '*') resultString = resultString.slice(0, resultString.length - 1);
+  
+  return resultString;
+};
+
+const asteriscIt = (n) => {
+  let rawString = (n + '');
+  let resultString = '';
+
+  if (Array.isArray(n)) {
+    rawString = rawString.replace(/,/g, '');
+  }
+  let absString = rawString;
+
+  if (n < 0) absString = rawString.slice(1);
+
+  for (let i = 0; i < absString.length; i += 1) {
+    resultString += absString[i];
+    if (absString[i] % 2 === 0) resultString += '*';
+  }
+
+  if (n < 0) resultString = '-' + resultString;
+
+  if (resultString[resultString.length - 1] === '*') resultString = resultString.slice(0, resultString.length - 1);
+
+  return resultString;
+};
+
+module.exports = asteriscIt;
---
+++ test/asterisk-it.test.js
@@ -0,0 +1,40 @@
+const { assert } = require('chai');
+
+const asteriscIt = require('../kata/asterisk-it');
+
+describe('Asterisk It', () => {
+  it('should be a function', (done) => {
+    assert.typeOf(asteriscIt, 'function');
+    done();
+  });
+  describe('should returns expected value', () => {
+    it('should parse assume of the template', (done) => {
+      assert.equal(asteriscIt(2222), '2*2*2*2');
+      done();
+    });
+    it('should parse assume of the template', (done) => {
+      assert.equal(asteriscIt(1111), '1111');
+      done();
+    });
+    it('should parse assume of the template', (done) => {
+      assert.equal(asteriscIt(9999), '9999');
+      done();
+    });
+    it('should parse assume of the template', (done) => {
+      assert.equal(asteriscIt('0000'), '0*0*0*0');
+      done();
+    });
+    it('should parse assume of the template', (done) => {
+      assert.equal(asteriscIt([1, 4, 64, 68, 67, 23, 1]), '14*6*4*6*8*67231');
+      done();
+    });
+    it('should parse assume of the template', (done) => {
+      assert.equal(asteriscIt(5312708), '531270*8');
+      done();
+    });
+    it('should parse assume of the template', (done) => {
+      assert.equal(asteriscIt(9682135), '96*8*2135');
+      done();
+    });
+  });
+});
