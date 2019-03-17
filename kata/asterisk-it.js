dbf6d1319fd20e32c312f43c670cc40240d40b96
Dmitry G. Anderson <dandgerson@gmail.com>
2019-03-17 03:37:58 +0300

    fix code style

--- kata/asterisk-it.js
+++ kata/asterisk-it.js
@@ -1,40 +1,39 @@
 const asteriscItArray = (n) => {
-  console.log(n);
-  let rawString = (n + '');
+  const rawString = (`${n}`);
   let resultString = '';
-  
+
   if (Array.isArray(n)) {
-    let rawArray = rawString.split(',');
-    let absArray = rawArray.slice();
-      
-    if (n[0] < 0)  absArray[0] = -n[0];
-      
+    const rawArray = rawString.split(',');
+    const absArray = rawArray.slice();
+
+    if (n[0] < 0) absArray[0] = -n[0];
+
     for (let i = 0; i < absArray.length; i += 1) {
       resultString += absArray[i];
       if (+absArray[i] % 2 === 0) resultString += '*';
     }
-    
-    if (n[0] < 0) resultString = '-' + resultString;
+
+    if (n[0] < 0) resultString = `-${resultString}`;
   } else {
     let absString = rawString;
-  
+
     if (n < 0) absString = rawString.slice(1);
-    
+
     for (let i = 0; i < absString.length; i += 1) {
       resultString += absString[i];
       if (absString[i] % 2 === 0) resultString += '*';
     }
-    
-    if (n < 0) resultString = '-' + resultString;
+
+    if (n < 0) resultString = `-${resultString}`;
   }
-  
+
   if (resultString[resultString.length - 1] === '*') resultString = resultString.slice(0, resultString.length - 1);
-  
+
   return resultString;
 };
 
 const asteriscIt = (n) => {
-  let rawString = (n + '');
+  let rawString = (`${n}`);
   let resultString = '';
 
   if (Array.isArray(n)) {
@@ -49,11 +48,11 @@ const asteriscIt = (n) => {
     if (absString[i] % 2 === 0) resultString += '*';
   }
 
-  if (n < 0) resultString = '-' + resultString;
+  if (n < 0) resultString = `-${resultString}`;
 
   if (resultString[resultString.length - 1] === '*') resultString = resultString.slice(0, resultString.length - 1);
 
   return resultString;
 };
 
-module.exports = asteriscIt;
+module.exports = asteriscItArray;
