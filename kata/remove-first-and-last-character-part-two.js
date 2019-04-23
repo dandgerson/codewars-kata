82990545054b094982c6d4c1ee7da9dc093f7f07
Dmitry G. Anderson <dandgerson@gmail.com>
2019-04-23 19:58:24 +0300

    refactor remove-first-and-last-char-part-two

--- kata/remove-first-and-last-character-part-two.js
+++ kata/remove-first-and-last-character-part-two.js
@@ -1,10 +1,8 @@
 const removeChars = (arr) => {
   if (!arr || !/\,/.test(arr) || /\s/.test(arr)) return null;
-
   const splitted = arr.split(',');
   if (splitted.length < 3) return null;
-  const trimmed = splitted.slice(1, splitted.length - 1);
-  return trimmed.toString().replace(/\,/g, ' ');
+  return splitted.slice(1, -1).toString().replace(/\,/g, ' ');
 };
 
 module.exports = removeChars;
